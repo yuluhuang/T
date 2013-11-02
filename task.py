27757710a03f8ef62b5b9179457613494e478e93
yuluhuang <504367857@qq.com>
2013-11-02 19:19:38 +0800

    Create task.py

---
+++ task.py
@@ -0,0 +1,7 @@
+import writetotxt
+import download
+import time
+
+writetotxt.writetotxtt(parrent=r'(?ui)data-src="(.*?\.png)"',url=r'http://octodex.github.com')	
+time.sleep(1)
+download.downloadd(txt1="1.txt",path='./pictures/',houz='.png')
